2d5b369756001eb145030188ad88993d750b38ff
Sumit Kumar <sk@outlook.com>
2015-11-15 10:59:31 +0100

    marker can now be removed

--- leaflet.pm.js
+++ leaflet.pm.js
@@ -96,6 +96,7 @@ L.PM.Poly = L.Handler.extend({
 
         marker.on('drag', this._onMarkerDrag, this);
         marker.on('dragend', this._onMarkerDragEnd, this);
+        marker.on('contextmenu', this._removeMarker, this);
 
         this._markerGroup.addLayer(marker);
 
@@ -104,7 +105,7 @@ L.PM.Poly = L.Handler.extend({
     },
 
     _createMiddleMarker: function(leftM, rightM) {
-
+        var self = this;
         var latlng = this._calcMiddleLatLng(leftM, rightM);
 
         var middleMarker = this._createMarker(latlng);
@@ -113,6 +114,93 @@ L.PM.Poly = L.Handler.extend({
         // save middle markers to the other markers
         leftM._middleMarkerRight = middleMarker;
         rightM._middleMarkerLeft = middleMarker;
+
+
+        middleMarker.on('dragstart', function() {
+            self._addMarker(middleMarker, leftM, rightM);
+        });
+        middleMarker.on('click', function() {
+            self._addMarker(middleMarker, leftM, rightM);
+        });
+
+
+    },
+
+    _addMarker: function(newM, leftM, rightM) {
+
+        // first, make this middlemarker a regular marker
+        newM.setOpacity(1);
+        newM.off('dragstart');
+        newM.off('click');
+
+        // now, create the polygon coordinate point for that marker
+        var latlng = newM.getLatLng();
+        var coords = this._poly._latlngs[0];
+        var index = leftM._index + 1;
+
+        coords.splice(index, 0, latlng);
+
+        // associate polygon coordinate with marker coordinate
+        newM._origLatLng = coords[index];
+
+        // push into marker array update the indexes for every marker
+        this._markers.splice(index, 0, newM);
+        for(i=0;i<this._markers.length;i++) {
+            this._markers[i]._index = i;
+        }
+
+        // create the new middlemarkers
+        this._createMiddleMarker(leftM, newM);
+        this._createMiddleMarker(newM, rightM);
+
+
+    },
+
+    _removeMarker: function(e) {
+        var marker = e.target;
+
+        console.log(marker._index);
+
+        // only continue if this is NOT a middle marker (those can't be deleted)
+        if(marker._index !== undefined) {
+
+            // remove polygon coordinate from this marker
+            var coords = this._poly._latlngs[0];
+            var index = marker._index;
+
+            coords.splice(index, 1);
+            this._poly.redraw();
+
+            // remove the marker and the middlemarkers next to it from the map
+            this._markerGroup.removeLayer(marker._middleMarkerLeft);
+            this._markerGroup.removeLayer(marker._middleMarkerRight);
+            this._markerGroup.removeLayer(marker);
+
+
+            // create the new middlemarker
+            var leftMarkerIndex = index - 1 < 0 ? this._markers.length - 1 : index - 1;
+            var rightMarkerIndex = index + 1 >= this._markers.length ? 0 : index + 1;
+
+            var leftM = this._markers[leftMarkerIndex];
+            var rightM = this._markers[rightMarkerIndex];
+            this._createMiddleMarker(leftM, rightM);
+
+
+            // remove the marker from the markers array
+            this._markers.splice(index, 1);
+
+            // update the remaining markers indexes
+            for(i=0;i<this._markers.length;i++) {
+                this._markers[i]._index = i;
+            }
+
+
+
+
+        }
+
+
+
 
 
     },
@@ -131,6 +219,7 @@ L.PM.Poly = L.Handler.extend({
 
         // update middle markers on the left and right
         // be aware that "left" and "right" might be interchanged, depending on the geojson array
+        // TODO rename "left" and "right" to "prev" and "next"
         var middleMarkerRightLatLng = this._calcMiddleLatLng(marker, this._markers[nextMarkerIndex]);
         marker._middleMarkerRight.setLatLng(middleMarkerRightLatLng);
 
