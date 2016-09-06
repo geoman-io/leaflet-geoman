0e061546393c39cb11f2b7d4551596520f39c96a
Sumit Kumar <sk@outlook.com>
2016-09-06 09:41:34 +0200

    added debug lines

--- src/js/Mixins/Snapping.js
+++ src/js/Mixins/Snapping.js
@@ -6,6 +6,11 @@ var SnapMixin = {
         this._markers.forEach((marker) => {
             marker.on('movestart', this._createSnapList, this);
             marker.on('move', this._handleSnapping, this);
+            marker.on('moveend', () => {
+                this.debugIndicatorLines.forEach((line) => {
+                    line.remove();
+                });
+            });
         });
 
     },
@@ -15,11 +20,13 @@ var SnapMixin = {
     _createSnapList: function() {
 
         let layers = [];
+        let debugIndicatorLines = [];
 
         // find all layers that are or inherit from Polylines
-        this._poly._map.eachLayer(function(layer) {
+        this._poly._map.eachLayer((layer) => {
             if(layer instanceof L.Polyline) {
                 layers.push(layer);
+                debugIndicatorLines.push(L.polyline([], {color: 'red'}).addTo(this._poly._map));
             }
         });
 
@@ -29,35 +36,51 @@ var SnapMixin = {
         });
 
         this._snapList = layers;
+        this.debugIndicatorLines = debugIndicatorLines;
     },
     _getClosestLayer: function(latlng, layers) {
 
-        layers.forEach((layer) => {
-            this._getClosestLayerPoint(latlng, layer);
-        })
+        let closestPolygonCoords = [];
+
+        layers.forEach((layer, index) => {
+            var closestLatLng = this._getClosestLayerLatlng(latlng, layer);
+            closestPolygonCoords.push(closestLatLng);
+            this.debugIndicatorLines[index].setLatLngs([latlng, closestLatLng]);
+        });
 
     },
-    _getClosestLayerPoint: function(latlng, layer) {
+    _getClosestLayerLatlng: function(latlng, layer) {
+        let map = this._poly._map;
 
-        let P = this._poly._map.latLngToLayerPoint(latlng);
+        // the point which we want to snap (probpably the marker that is dragged)
+        let P = map.latLngToLayerPoint(latlng);
+
+        // the coords of the layer
         let coords = layer.getLatLngs()[0];
 
+        // temp var for the shortest distance
         let shortestDistance;
 
+        // temp var for the shortest segment (line between two points) of the layer (polygon)
         let closestSegment;
 
+        // loop through the coords of the layer
         coords.forEach((point, index) => {
+            // take this (A) and the next (B) coord
             let nextIndex = index + 1 === coords.length ? 0 : index + 1;
-            let A = this._poly._map.latLngToLayerPoint(point);
-            let B = this._poly._map.latLngToLayerPoint(coords[nextIndex]);
+            let A = map.latLngToLayerPoint(point);
+            let B = map.latLngToLayerPoint(coords[nextIndex]);
 
+            // calc the distance between A and B
             let distance = L.LineUtil.pointToSegmentDistance(P, A, B);
 
+            // was shortestDistance set? if no, set it now
             if(!shortestDistance) {
                 shortestDistance = distance;
                 closestSegment = [A, B];
             }
 
+            // is the distance shorter than the previous one? Save it and the segment
             if(distance < shortestDistance) {
                 shortestDistance = distance;
                 closestSegment = [A, B];
@@ -65,9 +88,13 @@ var SnapMixin = {
 
         });
 
+        // console.log(closestSegment);
+
+        // L.polyline([map.layerPointToLatLng(closestSegment[0]), map.layerPointToLatLng(closestSegment[1])], {color: 'red'}).addTo(map)
+
         let closestPoint = L.LineUtil.closestPointOnSegment(latlng, closestSegment[0], closestSegment[1]);
 
-        console.log(closestPoint);
+        return map.layerPointToLatLng(closestPoint);
 
     }
 }
