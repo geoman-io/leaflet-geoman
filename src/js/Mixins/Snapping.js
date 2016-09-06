951100e5afae15c30a3a298f5c929fad4d6853e9
Sumit Kumar <sk@outlook.com>
2016-09-06 15:55:37 +0200

    OH MY GOD SHITS WORKING
    without Maximum call stack size exceeded error 💪

--- src/js/Mixins/Snapping.js
+++ src/js/Mixins/Snapping.js
@@ -1,11 +1,9 @@
 var SnapMixin = {
     _initSnappableMarkers: function() {
 
-        console.log('init');
-
         this._markers.forEach((marker) => {
             marker.on('movestart', this._createSnapList, this);
-            marker.on('move', this._handleSnapping, this);
+            marker.on('drag', this._handleSnapping, this);
             marker.on('moveend', () => {
                 this.debugIndicatorLines.forEach((line) => {
                     line.remove();
@@ -15,14 +13,24 @@ var SnapMixin = {
 
     },
     _handleSnapping: function(e) {
-        this._getClosestLayer(e.target.getLatLng(), this._snapList);
+
+        let marker = e.target;
+        let {layer, closestLatLng, distance} = this._calcClosestLayer(marker.getLatLng(), this._snapList);
+
+        // minimal distance before marker snaps (in pixels)
+        let minDistance = 30;
+        if(distance < minDistance) {
+            marker.setLatLng(closestLatLng);
+            this._onMarkerDrag(e);
+        }
+
     },
     _createSnapList: function() {
 
         let layers = [];
         let debugIndicatorLines = [];
 
-        // find all layers that are or inherit from Polylines
+        // find all layers that are or inherit from Polylines...
         this._poly._map.eachLayer((layer) => {
             if(layer instanceof L.Polyline) {
                 layers.push(layer);
@@ -30,7 +38,7 @@ var SnapMixin = {
             }
         });
 
-        // except myself
+        // ...except myself
         layers = layers.filter((layer) => {
             return this._poly !== layer
         });
@@ -38,21 +46,45 @@ var SnapMixin = {
         this._snapList = layers;
         this.debugIndicatorLines = debugIndicatorLines;
     },
-    _getClosestLayer: function(latlng, layers) {
+    _calcClosestLayer: function(latlng, layers) {
+        let map = this._poly._map;
 
-        let closestPolygonCoords = [];
+        let closestPolygon;
+        let closestLatLng;
+        let closestDistance;
+
+
 
         layers.forEach((layer, index) => {
-            var closestLatLng = this._getClosestLayerLatlng(latlng, layer);
-            closestPolygonCoords.push(closestLatLng);
-            this.debugIndicatorLines[index].setLatLngs([latlng, closestLatLng]);
+            let closestLatLngOfPoly = this._getClosestLayerLatlng(latlng, layer);
+
+            this.debugIndicatorLines[index].setLatLngs([latlng, closestLatLngOfPoly]);
+
+            let P = map.latLngToLayerPoint(latlng);
+            let C = map.latLngToLayerPoint(closestLatLngOfPoly);
+
+            let distance = P.distanceTo(C);
+
+            if(closestDistance === undefined || distance < closestDistance) {
+                closestDistance = distance;
+                closestLatLng = closestLatLngOfPoly;
+                closestPolygon = layer;
+            }
+
         });
+
+        return {
+            layer: closestPolygon,
+            closestLatLng,
+            distance: closestDistance
+        };
+
 
     },
     _getClosestLayerLatlng: function(latlng, layer) {
         let map = this._poly._map;
 
-        // the point which we want to snap (probpably the marker that is dragged)
+        // the point which we want to snap (probpably the marker that is dragged) in pixels
         let P = map.latLngToLayerPoint(latlng);
 
         // the coords of the layer
@@ -69,22 +101,16 @@ var SnapMixin = {
             // take this (A) and the next (B) coord
             let nextIndex = index + 1 === coords.length ? 0 : index + 1;
             let A = map.latLngToLayerPoint(point);
+
             let B = map.latLngToLayerPoint(coords[nextIndex]);
 
             // calc the distance between P and AB-segment
             let distance = L.LineUtil.pointToSegmentDistance(P, A, B);
 
-            // was shortestDistance set? if no, set it now
-            if(!shortestDistance) {
-                shortestDistance = distance;
-                closestSegment = [A, B];
-            }
-
             // is the distance shorter than the previous one? Save it and the segment
-            if(distance < shortestDistance) {
+            if(shortestDistance === undefined || distance < shortestDistance) {
                 shortestDistance = distance;
                 closestSegment = [A, B];
-
             }
 
         });
