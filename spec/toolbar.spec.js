fa706abf3e12c9d961318348d34e3f76db0c9cd3
Sumit Kumar <sk@outlook.com>
2016-10-18 20:15:31 +0200

    test toolbar options

--- spec/toolbar.spec.js
+++ spec/toolbar.spec.js
@@ -2,14 +2,23 @@
 const test = require('tape');
 const L = require('leaflet');
 
-test('is toolbar added', (t) => {
+test('TOOLBAR', (t) => {
     const mapContainer = document.createElement('DIV');
     document.body.appendChild(mapContainer);
     const map = L.map(mapContainer);
 
-    map.pm.addControls();
+    map.pm.addControls({
+        deleteLayer: false,
+    });
 
     const toolbar = mapContainer.querySelector('.leaflet-pm-toolbar');
     t.ok(document.body.contains(toolbar), 'Toolbar is in DOM');
+
+    const drawPolyButton = toolbar.querySelector('.icon-polygon');
+    t.ok(document.body.contains(drawPolyButton), 'Polygon Button is added');
+
+    const deleteLayerButton = toolbar.querySelector('.icon-delete');
+    t.notOk(document.body.contains(deleteLayerButton), 'Remove Button is not added');
+
     t.end();
 });
