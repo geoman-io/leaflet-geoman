f3062ade555c055baeee246744787432be8cd820
Sumit Kumar <sk@outlook.com>
2016-10-19 18:11:02 +0200

    added toolbar position check

--- spec/toolbar.spec.js
+++ spec/toolbar.spec.js
@@ -8,11 +8,16 @@ test('TOOLBAR', (t) => {
     const map = L.map(mapContainer);
 
     map.pm.addControls({
+        position: 'bottomright',
         deleteLayer: false,
     });
 
     const toolbar = mapContainer.querySelector('.leaflet-pm-toolbar');
     t.ok(document.body.contains(toolbar), 'Toolbar is in DOM');
+
+    const toolbarPosBottom = toolbar.parentNode.classList.contains('leaflet-bottom');
+    const toolbarPosRight = toolbar.parentNode.classList.contains('leaflet-right');
+    t.ok((toolbarPosRight && toolbarPosBottom), 'Toolbar is correctly positioned');
 
     const drawPolyButton = toolbar.querySelector('.icon-polygon');
     t.ok(document.body.contains(drawPolyButton), 'Polygon Button is added');
