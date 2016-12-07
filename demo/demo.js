d88ad47f675ed2a0566382c8f528da5e64c5b79b
Sumit Kumar <sk@outlook.com>
2016-12-07 12:11:31 +0100

    fix #100

--- demo/demo.js
+++ demo/demo.js
@@ -1,38 +1,60 @@
-var map2 = L.map('example2').setView([51.505, -0.09], 13);
+const map2 = L.map('example2').setView([51.505, -0.09], 13);
 
-map2.on('pm:create', function(e) {
+map2.on('pm:create', (e) => {
     alert('pm:create event fired. See console for details');
     console.log(e);
 });
 
-var m1 = L.marker([51.50313, -0.091223]);
-var m2 = L.marker([51.50614, -0.0989]);
-var m3 = L.marker([51.50915, -0.096112]);
+const m1 = L.marker([51.50313, -0.091223]);
+const m2 = L.marker([51.50614, -0.0989]);
+const m3 = L.marker([51.50915, -0.096112]);
 
-var mGroup = L.layerGroup([m1, m2, m3]).addTo(map2);
-console.log(mGroup);
+const mGroup = L.layerGroup([m1, m2, m3]).addTo(map2);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map2);
 
-var map3 = L.map('example3').setView([51.505, -0.09], 13);
+const map3 = L.map('example3').setView([51.505, -0.09], 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map3);
 
-var map4 = L.map('example4').setView([51.505, -0.09], 13);
+const map4 = L.map('example4').setView([51.505, -0.09], 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map4);
 
 map2.pm.addControls({
+    drawMarker: false,
     drawPolygon: true,
-    drawPolyline: true,
+    editPolygon: false,
+    drawPolyline: false,
     deleteLayer: true,
 });
+// map2.pm.addControls({
+//     drawMarker: false,
+//     drawPolygon: true,
+//     editPolygon: false,
+//     drawPolyline: false,
+//     deleteLayer: false,
+// });
+// map2.pm.addControls({
+//     drawMarker: true,
+//     drawPolygon: false,
+//     editPolygon: false,
+//     drawPolyline: false,
+//     deleteLayer: true,
+// });
+// map2.pm.addControls({
+//     drawMarker: true,
+//     drawPolygon: false,
+//     editPolygon: false,
+//     drawPolyline: true,
+//     deleteLayer: false,
+// });
 
 map2.pm.enableDraw('Poly', {
     templineStyle: {
@@ -48,174 +70,174 @@ map2.pm.enableDraw('Poly');
 
 // GEOSJON EXAMPLE
 
-var geoJsonData = {
-    "type": "FeatureCollection",
-    "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -0.15483856201171872,
-              51.527329038465936
-            ],
-            [
-              -0.16977310180664062,
-              51.51643437722083
-            ],
-            [
-              -0.15964508056640625,
-              51.50094238217541
-            ],
-            [
-              -0.13149261474609375,
-              51.5042549065934
-            ],
-            [
-              -0.11758804321289061,
-              51.518463972439385
-            ],
-            [
-              -0.13303756713867188,
-              51.53106680201548
-            ],
-            [
-              -0.15483856201171872,
-              51.527329038465936
-            ]
-          ]
-        ]
-      }
-    }
-    ]
+const geoJsonData = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [
+                    [
+                        [
+                            -0.15483856201171872,
+                            51.527329038465936,
+                        ],
+                        [
+                            -0.16977310180664062,
+                            51.51643437722083,
+                        ],
+                        [
+                            -0.15964508056640625,
+                            51.50094238217541,
+                        ],
+                        [
+                            -0.13149261474609375,
+                            51.5042549065934,
+                        ],
+                        [
+                            -0.11758804321289061,
+                            51.518463972439385,
+                        ],
+                        [
+                            -0.13303756713867188,
+                            51.53106680201548,
+                        ],
+                        [
+                            -0.15483856201171872,
+                            51.527329038465936,
+                        ],
+                    ],
+                ],
+            },
+        },
+    ],
 };
-var geoJsonButton = document.getElementById('test-geojson');
-var geoJsonLayer = L.geoJson().addTo(map3);
+const geoJsonButton = document.getElementById('test-geojson');
+const geoJsonLayer = L.geoJson().addTo(map3);
 geoJsonLayer.addData(geoJsonData);
 geoJsonLayer.pm.toggleEdit({
     draggable: true,
-    snappable: true
+    snappable: true,
 });
-var bounds = geoJsonLayer.getBounds();
+const bounds = geoJsonLayer.getBounds();
 map3.fitBounds(bounds);
-geoJsonLayer.addEventListener('click', function() {
+geoJsonLayer.addEventListener('click', () => {
     geoJsonLayer.pm.toggleEdit();
 });
 
-geoJsonLayer.on('pm:edit', function(e) {
+geoJsonLayer.on('pm:edit', (e) => {
     console.log(e);
 });
 
-geoJsonLayer.on('pm:dragstart', function(e) {
+geoJsonLayer.on('pm:dragstart', (e) => {
     console.log(e);
 });
-geoJsonLayer.on('pm:drag', function(e) {
+geoJsonLayer.on('pm:drag', (e) => {
     // console.log(e);
 });
-geoJsonLayer.on('pm:dragend', function(e) {
+geoJsonLayer.on('pm:dragend', (e) => {
     console.log(e);
 });
 
 
 // Polygon Example
 
-var polygonLayer = L.polygon([
+const polygonLayer = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
-    [51.51, -0.047]
+    [51.51, -0.047],
 ]).addTo(map3);
 polygonLayer.pm.toggleEdit();
 
 
 // Layer Group Example
 
-var layerGroupItem1 = L.polyline([
+const layerGroupItem1 = L.polyline([
     [51.51, -0.09],
     [51.513, -0.08],
-    [51.514, -0.11]
+    [51.514, -0.11],
 ]);
-var layerGroupItem2 = L.polygon([
+const layerGroupItem2 = L.polygon([
     [51.52, -0.06],
     [51.51, -0.07],
-    [51.52, -0.05]
+    [51.52, -0.05],
 ]);
 
-var layerGroupItem3 = L.polygon([
-  [
-    51.51549835365031,
-    -0.06450164634969281
-  ],
-  [
-    51.51944818307178,
-    -0.08425079345703125
-  ],
-  [
-    51.51868369995795,
-    -0.06131630004205801
-  ],
-  [
-    51.51549835365031,
-    -0.06450164634969281
-  ]
+const layerGroupItem3 = L.polygon([
+    [
+        51.51549835365031,
+        -0.06450164634969281,
+    ],
+    [
+        51.51944818307178,
+        -0.08425079345703125,
+    ],
+    [
+        51.51868369995795,
+        -0.06131630004205801,
+    ],
+    [
+        51.51549835365031,
+        -0.06450164634969281,
+    ],
 ]);
-var layerGroupItem4 = L.polygon([
-  [
-    51.51549835365031,
-    -0.06450164634969281
-  ],
-  [
-    51.51944818307178,
-    -0.08425079345703125
-  ],
-  [
-    51.51868369995795,
-    -0.06131630004205801
-  ],
-  [
-    51.51549835365031,
-    -0.06450164634969281
-  ]
+const layerGroupItem4 = L.polygon([
+    [
+        51.51549835365031,
+        -0.06450164634969281,
+    ],
+    [
+        51.51944818307178,
+        -0.08425079345703125,
+    ],
+    [
+        51.51868369995795,
+        -0.06131630004205801,
+    ],
+    [
+        51.51549835365031,
+        -0.06450164634969281,
+    ],
 ]);
-var layerGroupItem5 = L.polygon([
-  [
-    51.51549835365031,
-    -0.06450164634969281
-  ],
-  [
-    51.51944818307178,
-    -0.08425079345703125
-  ],
-  [
-    51.51868369995795,
-    -0.06131630004205801
-  ],
-  [
-    51.51549835365031,
-    -0.06450164634969281
-  ]
+const layerGroupItem5 = L.polygon([
+    [
+        51.51549835365031,
+        -0.06450164634969281,
+    ],
+    [
+        51.51944818307178,
+        -0.08425079345703125,
+    ],
+    [
+        51.51868369995795,
+        -0.06131630004205801,
+    ],
+    [
+        51.51549835365031,
+        -0.06450164634969281,
+    ],
 ]);
 
-var layerGroup = L.featureGroup([layerGroupItem1]).addTo(map4);
+const layerGroup = L.featureGroup([layerGroupItem1]).addTo(map4);
 layerGroup.pm.toggleEdit({
     draggable: true,
     snappable: true,
-    snapDistance: 30
+    snapDistance: 30,
 });
 
-layerGroup.on('pm:snap', function(e) {
+layerGroup.on('pm:snap', (e) => {
     console.log('snap');
     console.log(e);
 });
-layerGroup.on('pm:unsnap', function(e) {
+layerGroup.on('pm:unsnap', (e) => {
     console.log('unsnap');
     console.log(e);
 });
 
 map4.pm.addControls({
-    position: 'topright'
+    position: 'topright',
 });
 
 layerGroup.addLayer(layerGroupItem2);
@@ -223,19 +245,19 @@ layerGroup.addLayer(layerGroupItem3);
 // layerGroup.addLayer(layerGroupItem4);
 // layerGroup.addLayer(layerGroupItem5);
 
-layerGroup.on('pm:dragstart', function(e) {
+layerGroup.on('pm:dragstart', (e) => {
     console.log(e);
 });
-layerGroup.on('pm:drag', function(e) {
+layerGroup.on('pm:drag', (e) => {
     console.log(e);
 });
-layerGroup.on('pm:dragend', function(e) {
+layerGroup.on('pm:dragend', (e) => {
     console.log(e);
 });
-layerGroup.on('pm:markerdragstart', function(e) {
+layerGroup.on('pm:markerdragstart', (e) => {
     console.log(e);
 });
-layerGroup.on('pm:markerdragend', function(e) {
+layerGroup.on('pm:markerdragend', (e) => {
     console.log(e);
 });
 
