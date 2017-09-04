e1b1a4f4db8e164fde11eeb9ffddb90843859533
Sumit Kumar <sk@outlook.com>
2017-09-04 10:14:04 +0200

    minor things

--- spec/map.spec.js
+++ spec/map.spec.js
@@ -3,7 +3,7 @@ const test = require('tape');
 const L = require('leaflet');
 
 test('MAP', (t) => {
-    t.plan(2);
+    t.plan(3);
     const mapContainer = document.createElement('DIV');
     document.body.appendChild(mapContainer);
     const map = L.map(mapContainer);
@@ -16,4 +16,8 @@ test('MAP', (t) => {
     map.pm.toggleGlobalEditMode();
 
     t.ok(map.pm.globalEditEnabled(), 'Global Edit Mode enabled');
+
+    map.pm.disableGlobalEditMode();
+
+    t.notOk(map.pm.globalEditEnabled(), 'Global Edit Mode disabled');
 });
