3c4a6857490d801fcee52e1d787015f4b65599af
Sumit Kumar <sk@outlook.com>
2017-09-22 17:50:36 +0200

    nothing

--- webpack.build.js
+++ webpack.build.js
@@ -36,5 +36,11 @@ module.exports = {
             },
         ],
     },
-    plugins: [new ExtractTextPlugin('leaflet.pm.css'), new webpack.optimize.UglifyJsPlugin()],
+    plugins: [
+        new ExtractTextPlugin('leaflet.pm.css'),
+        new webpack.optimize.UglifyJsPlugin(),
+        // new webpack.DefinePlugin({
+        //     'process.env.NODE_ENV': '"production"',
+        // }),
+    ],
 };
