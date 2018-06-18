c73739c80f6a44e20931bb2d8f7927ef34a5d8f9
Sumit Kumar <sk@outlook.com>
2018-06-18 10:46:22 +0200

    added small credit

--- src/js/Edit/L.PM.Edit.Line.js
+++ src/js/Edit/L.PM.Edit.Line.js
@@ -423,6 +423,7 @@ Edit.Line = Edit.extend({
         });
     },
     isEmptyDeep(l) {
+        // thanks for the function, Felix Heck
         const flatten = list =>
             list.filter(x => ![null, '', undefined].includes(x)).reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
 
