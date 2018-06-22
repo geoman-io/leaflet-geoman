d42315161d1354901be2cb32eac6aafb90e98b57
Sumit Kumar <sk@outlook.com>
2018-06-22 11:52:42 +0200

    added some marker test

--- cypress/integration/marker.spec.js
+++ cypress/integration/marker.spec.js
@@ -1,1 +1,23 @@
-describe('Draw Marker', () => {});
+describe('Draw Marker', () => {
+    const mapSelector = '#map';
+
+    it('places markers', () => {
+        cy.toolbarButton('marker').click();
+
+        cy.get(mapSelector)
+            .click(90, 250)
+            .click(150, 50)
+            .click(500, 50)
+            .click(500, 300);
+
+        cy.get('.leaflet-marker-icon').should(($p) => {
+            expect($p).to.have.length(5);
+        });
+
+        cy.toolbarButton('marker').click();
+
+        cy.get('.leaflet-marker-icon').should(($p) => {
+            expect($p).to.have.length(4);
+        });
+    });
+});
