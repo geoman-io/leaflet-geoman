2011648ca541e87c707cba0728a49427305f54f3
Sumit Kumar <sk@outlook.com>
2018-07-10 11:01:53 +0200

    Circle Options are now properly handled after draw (Patch) (#313)
    * started with the test and stuff
    
    * Circle now uses correct option when drawn. Fixes 304 (patch)
    
    * removed unneccary change
    
    * added another test for circle options

--- cypress/integration/circle.spec.js
+++ cypress/integration/circle.spec.js
@@ -19,4 +19,54 @@ describe('Draw Circle', () => {
         cy.hasVertexMarkers(2);
         cy.hasMiddleMarkers(0);
     });
+
+    it('uses correct options from enableDraw', () => {
+        cy.window().then(({ map, L }) => {
+            const options = {
+                pathOptions: {
+                    color: 'red',
+                    fillColor: 'orange',
+                    fillOpacity: 0.7,
+                    radius: 20,
+                },
+            };
+            map.pm.enableDraw('Circle', options);
+
+            cy.get(mapSelector)
+                .click(200, 200)
+                .click(250, 250)
+                .then(() => {
+                    map.eachLayer((layer) => {
+                        if (layer instanceof L.Circle) {
+                            expect(layer.options.color).to.equal('red');
+                            expect(layer.options.fillColor).to.equal('orange');
+                        }
+                    });
+                });
+        });
+    });
+
+    it('uses correct options from setPathOptions', () => {
+        cy.toolbarButton('circle')
+            .click()
+            .parent('a')
+            .should('have.class', 'active');
+
+        cy.window().then(({ map, L }) => {
+            map.pm.setPathOptions({
+                color: 'yellow',
+            });
+
+            cy.get(mapSelector)
+                .click(200, 200)
+                .click(250, 250)
+                .then(() => {
+                    map.eachLayer((layer) => {
+                        if (layer instanceof L.Circle) {
+                            expect(layer.options.color).to.equal('yellow');
+                        }
+                    });
+                });
+        });
+    });
 });
