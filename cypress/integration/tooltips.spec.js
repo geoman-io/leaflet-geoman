2769a948ef49700d41e287a0d7c5818ffb28c0c3
Sumit Kumar <sk@outlook.com>
2018-12-08 16:18:51 +0100

    added proper tooltip test

--- cypress/integration/tooltips.spec.js
+++ cypress/integration/tooltips.spec.js
@@ -3,24 +3,32 @@ describe('Shows Tooltips', () => {
 
     const mapSelector = '#map';
 
-    it('has tooltips', () => {
-        cy.window().then(({ map, L }) => {
-            // test pm:create event
-            Cypress.$(map).on('pm:create', ({ originalEvent: event }) => {
-                const poly = event.layer;
-            });
-        });
+    it('Has Polygon Tooltips', () => {
+        cy.get('.leaflet-tooltip-bottom').should('not.exist');
 
         // activate polygon drawing
         cy.toolbarButton('polygon').click();
 
-        // // draw a polygon
-        // cy.get(mapSelector)
-        //     .click(290, 250)
-        //     .click(300, 50)
-        //     .click(350, 50)
-        //     .click(350, 150)
-        //     .click(400, 150)
-        //     .click(290, 250);
+        cy.get('.leaflet-tooltip-bottom').should('exist');
+
+        cy.get('.leaflet-tooltip-bottom').then((el) => {
+            expect(el).to.have.text('Click to Place First Vertex');
+        });
+
+        // draw a polygon
+        cy.get(mapSelector)
+            .click(290, 250)
+            .click(300, 50)
+            .click(350, 50)
+            .click(350, 150)
+            .click(400, 150);
+
+        cy.get('.leaflet-tooltip-bottom').then((el) => {
+            expect(el).to.have.text('Click First Marker to Finish');
+        });
+
+        cy.get(mapSelector).click(290, 250);
+
+        cy.get('.leaflet-tooltip-bottom').should('not.exist');
     });
 });
