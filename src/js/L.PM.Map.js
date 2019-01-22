123885e9754dfb889dcca44b554d1c95d29c868b
Sumit Kumar <sk@outlook.com>
2019-01-22 20:02:13 +0100

    fixed linting errors

--- src/js/L.PM.Map.js
+++ src/js/L.PM.Map.js
@@ -1,151 +1,149 @@
 const Map = L.Class.extend({
-    initialize(map) {
-        this.map = map;
-        this.Draw = new L.PM.Draw(map);
-        this.Toolbar = new L.PM.Toolbar(map);
+  initialize(map) {
+    this.map = map;
+    this.Draw = new L.PM.Draw(map);
+    this.Toolbar = new L.PM.Toolbar(map);
 
-        this._globalRemovalMode = false;
-    },
-    addControls(options) {
-        this.Toolbar.addControls(options);
-    },
-    removeControls() {
-        this.Toolbar.removeControls();
-    },
-    toggleControls() {
-        this.Toolbar.toggleControls();
-    },
-    controlsVisible() {
-        return this.Toolbar.isVisible;
-    },
-    enableDraw(shape = 'Poly', options) {
-        this.Draw.enable(shape, options);
-    },
-    disableDraw(shape = 'Poly') {
-        this.Draw.disable(shape);
-    },
-    setPathOptions(options) {
-        this.Draw.setPathOptions(options);
-    },
-    removeLayer(e) {
-        const layer = e.target;
-        // only remove layer, if it's handled by leaflet.pm,
-        // not a tempLayer and not currently being dragged
-        const removeable =
-            !layer._pmTempLayer && (!layer.pm || !layer.pm.dragging());
+    this._globalRemovalMode = false;
+  },
+  addControls(options) {
+    this.Toolbar.addControls(options);
+  },
+  removeControls() {
+    this.Toolbar.removeControls();
+  },
+  toggleControls() {
+    this.Toolbar.toggleControls();
+  },
+  controlsVisible() {
+    return this.Toolbar.isVisible;
+  },
+  enableDraw(shape = 'Poly', options) {
+    this.Draw.enable(shape, options);
+  },
+  disableDraw(shape = 'Poly') {
+    this.Draw.disable(shape);
+  },
+  setPathOptions(options) {
+    this.Draw.setPathOptions(options);
+  },
+  removeLayer(e) {
+    const layer = e.target;
+    // only remove layer, if it's handled by leaflet.pm,
+    // not a tempLayer and not currently being dragged
+    const removeable =
+      !layer._pmTempLayer && (!layer.pm || !layer.pm.dragging());
 
-        if (removeable) {
-            layer.remove();
-            this.map.fire('pm:remove', { layer });
+    if (removeable) {
+      layer.remove();
+      this.map.fire('pm:remove', { layer });
+    }
+  },
+  toggleGlobalRemovalMode() {
+    // toggle global edit mode
+    if (this.globalRemovalEnabled()) {
+      this._globalRemovalMode = false;
+      this.map.eachLayer(layer => {
+        layer.off('click', this.removeLayer, this);
+      });
+    } else {
+      this._globalRemovalMode = true;
+      this.map.eachLayer(layer => {
+        if (
+          layer.pm &&
+          !(layer.pm.options && layer.pm.options.preventMarkerRemoval)
+        ) {
+          layer.on('click', this.removeLayer, this);
         }
-    },
-    toggleGlobalRemovalMode() {
-        // toggle global edit mode
-        if (this.globalRemovalEnabled()) {
-            this._globalRemovalMode = false;
-            this.map.eachLayer((layer) => {
-                layer.off('click', this.removeLayer, this);
-            });
-        } else {
-            this._globalRemovalMode = true;
-            this.map.eachLayer((layer) => {
-                if (
-                    layer.pm &&
-                    !(layer.pm.options && layer.pm.options.preventMarkerRemoval)
-                ) {
-                    layer.on('click', this.removeLayer, this);
-                }
-            });
-        }
+      });
+    }
 
-        // toogle the button in the toolbar
-        this.Toolbar.toggleButton('deleteLayer', this._globalRemovalMode);
-    },
-    globalRemovalEnabled() {
-        return this._globalRemovalMode;
-    },
-    globalEditEnabled() {
-        return this._globalEditMode;
-    },
-    enableGlobalEditMode(options) {
-        // find all layers handled by leaflet.pm
-        let layers = [];
-        this.map.eachLayer((layer) => {
-            if (
-                layer instanceof L.Polyline ||
-                layer instanceof L.Marker ||
-                layer instanceof L.Circle
-            ) {
-                layers.push(layer);
-            }
-        });
+    // toogle the button in the toolbar
+    this.Toolbar.toggleButton('deleteLayer', this._globalRemovalMode);
+  },
+  globalRemovalEnabled() {
+    return this._globalRemovalMode;
+  },
+  globalEditEnabled() {
+    return this._globalEditMode;
+  },
+  enableGlobalEditMode(options) {
+    // find all layers handled by leaflet.pm
+    let layers = [];
+    this.map.eachLayer(layer => {
+      if (
+        layer instanceof L.Polyline ||
+        layer instanceof L.Marker ||
+        layer instanceof L.Circle
+      ) {
+        layers.push(layer);
+      }
+    });
 
-        // filter out layers that don't have the leaflet.pm instance
-        layers = layers.filter(layer => !!layer.pm);
+    // filter out layers that don't have the leaflet.pm instance
+    layers = layers.filter(layer => !!layer.pm);
 
-        // filter out everything that's leaflet.pm specific temporary stuff
-        layers = layers.filter(layer => !layer._pmTempLayer);
+    // filter out everything that's leaflet.pm specific temporary stuff
+    layers = layers.filter(layer => !layer._pmTempLayer);
 
-        this._globalEditMode = true;
+    this._globalEditMode = true;
 
-        layers.forEach((layer) => {
-            // console.log(layer);
-            layer.pm.enable(options);
-        });
+    layers.forEach(layer => {
+      layer.pm.enable(options);
+    });
 
-        // toggle the button in the toolbar
-        this.Toolbar.toggleButton('editPolygon', this._globalEditMode);
+    // toggle the button in the toolbar
+    this.Toolbar.toggleButton('editPolygon', this._globalEditMode);
 
-        // fire event
-        this._fireEditModeEvent(true);
-    },
-    disableGlobalEditMode() {
-        // find all layers handles by leaflet.pm
-        let layers = [];
-        this.map.eachLayer((layer) => {
-            if (
-                layer instanceof L.Polyline ||
-                layer instanceof L.Marker ||
-                layer instanceof L.Circle
-            ) {
-                layers.push(layer);
-            }
-        });
+    // fire event
+    this._fireEditModeEvent(true);
+  },
+  disableGlobalEditMode() {
+    // find all layers handles by leaflet.pm
+    let layers = [];
+    this.map.eachLayer(layer => {
+      if (
+        layer instanceof L.Polyline ||
+        layer instanceof L.Marker ||
+        layer instanceof L.Circle
+      ) {
+        layers.push(layer);
+      }
+    });
 
-        // filter out layers that don't have the leaflet.pm instance
-        layers = layers.filter(layer => !!layer.pm);
+    // filter out layers that don't have the leaflet.pm instance
+    layers = layers.filter(layer => !!layer.pm);
 
-        // filter out everything that's leaflet.pm specific temporary stuff
-        layers = layers.filter(layer => !layer._pmTempLayer);
+    // filter out everything that's leaflet.pm specific temporary stuff
+    layers = layers.filter(layer => !layer._pmTempLayer);
 
-        this._globalEditMode = false;
+    this._globalEditMode = false;
 
-        layers.forEach((layer) => {
-            layer.pm.disable();
-        });
+    layers.forEach(layer => {
+      layer.pm.disable();
+    });
 
-        // toggle the button in the toolbar
-        this.Toolbar.toggleButton('editPolygon', this._globalEditMode);
+    // toggle the button in the toolbar
+    this.Toolbar.toggleButton('editPolygon', this._globalEditMode);
 
-        // fire event
-        this._fireEditModeEvent(false);
-    },
-    _fireEditModeEvent(enabled) {
-        this.map.fire('pm:globaleditmodetoggled', {
-            enabled,
-            map: this.map,
-        });
-    },
-    toggleGlobalEditMode(options) {
-        // console.log('toggle global edit mode', options);
-        if (this.globalEditEnabled()) {
-            // disable
-            this.disableGlobalEditMode();
-        } else {
-            // enable
-            this.enableGlobalEditMode(options);
-        }
-    },
+    // fire event
+    this._fireEditModeEvent(false);
+  },
+  _fireEditModeEvent(enabled) {
+    this.map.fire('pm:globaleditmodetoggled', {
+      enabled,
+      map: this.map,
+    });
+  },
+  toggleGlobalEditMode(options) {
+    if (this.globalEditEnabled()) {
+      // disable
+      this.disableGlobalEditMode();
+    } else {
+      // enable
+      this.enableGlobalEditMode(options);
+    }
+  },
 });
 
 export default Map;
