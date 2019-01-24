2bbce37762fac87eea49ed992e9c87d2b7315492
Sumit Kumar <sk@outlook.com>
2019-01-24 17:28:18 +0100

    started

--- src/js/Draw/L.PM.Draw.Line.js
+++ src/js/Draw/L.PM.Draw.Line.js
@@ -218,6 +218,8 @@ Draw.Line = Draw.extend({
     const coords = this._layer.getLatLngs();
     const removedCoord = coords.pop();
 
+    console.log('remove last vertex');
+
     // if all coords are gone, cancel drawing
     if (coords.length < 1) {
       this.disable();
