0a5b661d44039d6695c669c77021e7279d1361d8
Sumit Kumar <sk@outlook.com>
2019-01-24 16:38:57 +0100

    added failing test for edit mode

--- cypress/integration/globalmodes.spec.js
+++ cypress/integration/globalmodes.spec.js
@@ -21,7 +21,32 @@ describe('Removal Mode', () => {
     cy.toolbarButton('delete').click();
   });
 
-  it('re-applies onAdd of layers', () => {
+  it.only('re-applies edit mode onAdd', () => {
+    cy.toolbarButton('polygon').click();
+
+    const jsonString =
+      '{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[-0.155182,51.515687],[-0.155182,51.521028],[-0.124283,51.521028],[-0.124283,51.510345],[-0.155182,51.515687]]]}}';
+
+    const poly = JSON.parse(jsonString);
+
+    cy.get(mapSelector)
+      .click(320, 150)
+      .click(320, 100)
+      .click(400, 100)
+      .click(400, 200)
+      .click(320, 150);
+
+    cy.toolbarButton('edit').click();
+    cy.hasVertexMarkers(4);
+
+    cy.window().then(({ map, L }) => {
+      L.geoJSON(poly).addTo(map);
+    });
+
+    cy.hasVertexMarkers(8);
+  });
+
+  it('re-applies removal mode onAdd', () => {
     cy.toolbarButton('marker').click();
 
     cy.get(mapSelector)
