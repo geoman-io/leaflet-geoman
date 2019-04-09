4b3d7bb2f7f572e4a8761bfec8b2ab891fc151ae
Cosme Benito <cosme.benito@mapidea.co>
2019-04-09 10:06:04 +0100

    Updating code to meet main branch's changes

--- src/js/Draw/L.PM.Draw.CircleMarker.js
+++ src/js/Draw/L.PM.Draw.CircleMarker.js
@@ -1,109 +1,95 @@
 import Draw from './L.PM.Draw';
 
-Draw.CircleMarker = Draw.extend({
-    initialize(map) {
-        this._map = map;
-        this._shape = 'CircleMarker';
-        this.toolbarButtonName = 'drawCircleMarker';
-    },
-    enable(options) {
-        // TODO: Think about if these options could be passed globally for all
-        // instances of L.PM.Draw. So a dev could set drawing style one time as some kind of config
-        L.Util.setOptions(this, options);
+Draw.CircleMarker = Draw.Marker.extend({
+  initialize(map) {
+    this._map = map;
+    this._shape = 'CircleMarker';
+    this.toolbarButtonName = 'drawCircleMarker';
+  },
+  enable(options) {
+    // TODO: Think about if these options could be passed globally for all
+    // instances of L.PM.Draw. So a dev could set drawing style one time as some kind of config
+    L.Util.setOptions(this, options);
 
-        // enable draw mode
-        this._enabled = true;
+    // change enabled state
+    this._enabled = true;
 
+    // create a marker on click on the map
+    this._map.on('click', this._createMarker, this);
 
-        // this is the circle we want to draw
-        this._layer = L.circleMarker([0, 0], this.options.templineStyle);
-        this._layer._pmTempLayer = true;
-        this._hintMarker = this._layer;
-        this._hintMarker.addTo(this._map);
+    // toggle the draw button of the Toolbar in case drawing mode got enabled without the button
+    this._map.pm.Toolbar.toggleButton(this.toolbarButtonName, true);
 
-        // change map cursor
-        this._map._container.style.cursor = 'crosshair';
+    // this is the hintmarker on the mouse cursor
+    this._hintMarker = L.circleMarker([0, 0], this.options.templineStyle);
+    this._hintMarker._pmTempLayer = true;
+    this._hintMarker.addTo(this._map);
 
-        // create a polygon-point on click
-        this._map.on('click', this._finishShape, this);
+    // add tooltip to hintmarker
+    if (this.options.tooltips) {
+      this._hintMarker
+        .bindTooltip('Click to place circle marker', {
+          permanent: true,
+          offset: L.point(0, 10),
+          direction: 'bottom',
 
-        // sync hint marker with mouse cursor
-        this._map.on('mousemove', this._syncHintMarker, this);
+          opacity: 0.8,
+        })
+        .openTooltip();
+    }
 
-        // fire drawstart event
-        this._map.fire('pm:drawstart', { shape: this._shape, workingLayer: this._layer });
+    // this is just to keep the snappable mixin happy
+    this._layer = this._hintMarker;
 
-        // toggle the draw button of the Toolbar in case drawing mode got enabled without the button
-        this._map.pm.Toolbar.toggleButton(this.toolbarButtonName, true);
-    },
-    disable() {
-        // cancel, if drawing mode isn't event enabled
-        if (!this._enabled) {
-            return;
-        }
+    // sync hint marker with mouse cursor
+    this._map.on('mousemove', this._syncHintMarker, this);
 
-        this._enabled = false;
+    // fire drawstart event
+    this._map.fire('pm:drawstart', {
+      shape: this._shape,
+      workingLayer: this._layer,
+    });
 
-        // reset cursor
-        this._map._container.style.cursor = 'default';
+    // enable edit mode for existing markers
+    this._map.eachLayer(layer => {
+      if (this.isRelevantMarker(layer)) {
+        layer.pm.enable();
+      }
+    });
+  },
+  isRelevantMarker(layer) {
+    return layer instanceof L.CircleMarker && !(layer instanceof L.Circle) && layer.pm && !layer._pmTempLayer;
+  },
+  _createMarker(e) {
+    if (!e.latlng) {
+      return;
+    }
 
-        // unbind listeners
-        this._map.off('click', this._finishShape, this);
-        this._map.off('mousemove', this._syncHintMarker, this);
+    // assign the coordinate of the click to the hintMarker, that's necessary for
+    // mobile where the marker can't follow a cursor
+    if (!this._hintMarker._snapped) {
+      this._hintMarker.setLatLng(e.latlng);
+    }
 
-        // remove helping layers
-        this._map.removeLayer(this._layer);
+    // get coordinate for new vertex by hintMarker (cursor marker)
+    const latlng = this._hintMarker.getLatLng();
 
-        // fire drawend event
-        this._map.fire('pm:drawend', { shape: this._shape });
+    // create marker
+    const marker = L.circleMarker(latlng, this.options.pathOptions);
 
-        // toggle the draw button of the Toolbar in case drawing mode got disabled without the button
-        this._map.pm.Toolbar.toggleButton(this.toolbarButtonName, false);
+    // add marker to the map
+    marker.addTo(this._map);
 
-        // cleanup snapping
-        if (this.options.snappable) {
-            this._cleanupSnapping();
-        }
-    },
-    enabled() {
-        return this._enabled;
-    },
-    toggle(options) {
-        if (this.enabled()) {
-            this.disable();
-        } else {
-            this.enable(options);
-        }
-    },
-    _syncHintMarker(e) {
-        // move the cursor marker
-        this._hintMarker.setLatLng(e.latlng);
+    // enable editing for the marker
+    marker.pm.enable();
 
-        // if snapping is enabled, do it
-        if (this.options.snappable) {
-            const fakeDragEvent = e;
-            fakeDragEvent.target = this._hintMarker;
-            this._handleSnapping(fakeDragEvent);
-        }
-    },
-    _finishShape(event) {
-        if(!event.latlng) {
-            return;
-        }
+    // fire the pm:create event and pass shape and marker
+    this._map.fire('pm:create', {
+      shape: this._shape,
+      marker, // DEPRECATED
+      layer: marker,
+    });
 
-        // The CircleMarker coordinates
-        const center = event.latlng;
-
-        // disable drawing
-        this.disable();
-
-        // create the final circle layer
-        const circleLayer = L.circleMarker(center, this.options.pathOptions).addTo(this._map);
-
-        // fire the pm:create event and pass shape and layer
-        this._map.fire('pm:create', {
-            shape: this._shape,
-            layer: circleLayer,
-        });
-    },
+    this._cleanupSnapping();
+  },
 });
--- src/js/Edit/L.PM.Edit.CircleMarker.js
+++ src/js/Edit/L.PM.Edit.CircleMarker.js
@@ -1,141 +1,147 @@
 import Edit from './L.PM.Edit';
 
 Edit.CircleMarker = Edit.extend({
-    initialize(layer) {
-        this._layer = layer;
-        this._enabled = false;
-    },
-    toggleEdit(options) {
-        if (!this.enabled()) {
-            this.enable(options);
-        } else {
-            this.disable();
-        }
-    },
-    enabled() {
-        return this._enabled;
-    },
-    enable(options) {
-        L.Util.setOptions(this, options);
+  initialize(layer) {
+    this._layer = layer;
+    this._enabled = false;
+  },
+  toggleEdit(options) {
+    if (!this.enabled()) {
+      this.enable(options);
+    } else {
+      this.disable();
+    }
+  },
+  enabled() {
+    return this._enabled;
+  },
+  enable(options) {
+    L.Util.setOptions(this, options);
 
-        this._map = this._layer._map;
+    this._map = this._layer._map;
 
-        // cancel when map isn't available, this happens when the polygon is removed before this fires
-        if (!this._map) {
-            return;
-        }
+    // cancel when map isn't available, this happens when the polygon is removed before this fires
+    if (!this._map) {
+      return;
+    }
 
-        if (!this.enabled()) {
-            // if it was already enabled, disable first
-            // we don't block enabling again because new options might be passed
-            this.disable();
-        }
+    if (!this.enabled()) {
+      // if it was already enabled, disable first
+      // we don't block enabling again because new options might be passed
+      this.disable();
+    }
 
-        // change state
-        this._enabled = true;
+    // change state
+    this._enabled = true;
 
-        // // init markers
-        this._initMarkers();
+    // enable removal for the marker
+    if (!this.options.preventMarkerRemoval) {
+      this._layer.on('contextmenu', this._removeMarker, this);
+    }
 
-        // if polygon gets removed from map, disable edit mode
-        this._layer.on('remove', (e) => {
-            this.disable(e.target);
-        });
-    },
-    _initMarkers() {
-        const map = this._map;
+    // // init markers
+    this._initMarkers();
 
-        // cleanup old ones first
-        if (this._markerGroup) {
-            this._markerGroup.clearLayers();
-        }
+    // enable snapping
+    if (this.options.snappable) {
+      this._initSnappableMarkers();
+    }
+  },
+  _initMarkers() {
+    const map = this._map;
 
-        // add markerGroup to map, markerGroup includes regular and middle markers
-        this._markerGroup = new L.LayerGroup();
-        this._markerGroup._pmTempLayer = true;
-        map.addLayer(this._markerGroup);
+    // cleanup old ones first
+    if (this._markerGroup) {
+      this._markerGroup.clearLayers();
+    }
 
-        // create marker for each coordinate
-        const center = this._layer.getLatLng();
-        this._hintMarker = this._createHintMarker(center);
-        this._markers = [this._hintMarker];
+    // add markerGroup to map, markerGroup includes regular and middle markers
+    this._markerGroup = new L.LayerGroup();
+    this._markerGroup._pmTempLayer = true;
+    map.addLayer(this._markerGroup);
 
-        if (this.options.snappable) {
-            this._initSnappableMarkers();
-        }
-    },
-    disable(layer = this._layer) {
-        // if it's not enabled, it doesn't need to be disabled
-        if (!this.enabled()) {
-            return false;
-        }
+    // create marker for each coordinate
+    const center = this._layer.getLatLng();
+    this._hintMarker = this._createHintMarker(center);
+    this._markers = [this._hintMarker];
 
-        // prevent disabling if layer is being dragged
-        if (layer.pm._dragging) {
-            return false;
-        }
-        layer.pm._enabled = false;
-        layer.pm._markerGroup.clearLayers();
+    if (this.options.snappable) {
+      this._initSnappableMarkers();
+    }
+  },
+  disable(layer = this._layer) {
+    // if it's not enabled, it doesn't need to be disabled
+    if (!this.enabled()) {
+      return false;
+    }
+
+    // prevent disabling if layer is being dragged
+    if (layer.pm._dragging) {
+      return false;
+    }
+    layer.pm._enabled = false;
+    layer.pm._markerGroup.clearLayers();
 
 
-        // clean up events
-        this._hintMarker.off('move', this._moveMarker, this);
-        this._hintMarker.off('dragend', this._onMarkerDragEnd, this);
-        this._hintMarker.off('contextmenu', this._removeMarker, this);
+    // clean up events
+    this._hintMarker.off('move', this._moveMarker, this);
+    this._hintMarker.off('dragend', this._onMarkerDragEnd, this);
+    this._hintMarker.off('contextmenu', this._removeMarker, this);
+    this._map.removeLayer(this._layerGroup);
 
-        // remove draggable class
-        if (layer._path) {
-            const el = layer._path;
-            L.DomUtil.removeClass(el, 'leaflet-pm-draggable');
-        }
+    // remove draggable class
+    if (layer._path) {
+      const el = layer._path;
+      L.DomUtil.removeClass(el, 'leaflet-pm-draggable');
+    }
 
-        if (this._layerEdited) {
-            this._layer.fire('pm:update', {});
-        }
-        this._layerEdited = false;
+    if (this._layerEdited) {
+      this._layer.fire('pm:update', {});
+    }
+    this._layerEdited = false;
 
-        return true;
-    },
-    _moveMarker(e) {
-        const center = e.latlng;
-        this._layer.setLatLng(center).redraw();
-    },
-    _createHintMarker(latlng) {
-        const marker = new L.Marker(latlng, {
-            draggable: true,
-            icon: L.divIcon({ className: 'marker-icon' }),
-        });
+    return true;
+  },
+  _moveMarker(e) {
+    const center = e.latlng;
+    this._layer.setLatLng(center).redraw();
+  },
+  _createHintMarker(latlng) {
+    const marker = new L.Marker(latlng, {
+      draggable: true,
+      icon: L.divIcon({ className: 'marker-icon' }),
+    });
 
-        marker._pmTempLayer = true;
-        marker._origLatLng = latlng;
+    marker._pmTempLayer = true;
+    marker._origLatLng = latlng;
 
-        marker.on('move', this._moveMarker, this);
-        marker.on('dragend', this._onMarkerDragEnd, this);
-        marker.on('contextmenu', this._removeMarker, this);
+    marker.on('move', this._moveMarker, this);
+    marker.on('dragend', this._onMarkerDragEnd, this);
+    marker.on('contextmenu', this._removeMarker, this);
 
-        this._markerGroup.addLayer(marker);
+    this._markerGroup.addLayer(marker);
 
-        return marker;
-    },
-    _removeMarker() {
-        this._layer.fire('pm:remove');
-        this._layer.remove();
-    },
-    _fireEdit() {
-        // fire edit event
-        this._layer.fire('pm:edit');
-        this._layerEdited = true;
-    },
-    _onMarkerDragStart(e) {
-        this._layer.fire('pm:markerdragstart', {
-            markerEvent: e,
-        });
-    },
-    _onMarkerDragEnd(e) {
-        this._layer.fire('pm:markerdragend', {
-            markerEvent: e,
-        });
+    return marker;
+  },
+  _removeMarker() {
+    this._layer.fire('pm:remove');
+    this._layer.remove();
+  },
+  _fireEdit() {
+    // fire edit event
+    this._layer.fire('pm:edit');
+    this._layerEdited = true;
+  },
+  _onMarkerDragStart(e) {
+    this._layer.fire('pm:markerdragstart', {
+      markerEvent: e,
+    });
+  },
+  _onMarkerDragEnd(e) {
+    this._layer.fire('pm:markerdragend', {
+      markerEvent: e,
+    });
 
-        this._fireEdit();
-    },
+    this._fireEdit();
+  },
 });
