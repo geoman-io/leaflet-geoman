706dbc693dd15422e0f6b8f4a54e4d3443430ee4
Jumpy <giampychan@yahoo.it>
2019-05-04 18:01:26 +0200

    added Italian translation file (#426) (patch)
    * Added Italian translation
    
    * Update index.js
    
    * Update README.md

--- src/assets/translations/index.js
+++ src/assets/translations/index.js
@@ -1,11 +1,13 @@
 import en from './en.json';
 import de from './de.json';
+import it from './it.json';
 import ro from './ro.json';
 import ru from './ru.json';
 
 export default {
   en,
   de,
+  it,
   ro,
   ru,
 };
