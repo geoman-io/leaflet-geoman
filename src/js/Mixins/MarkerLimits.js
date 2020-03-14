58388a723106bdddbf610fc0e0e1e418ac228077
Sumit Kumar <sk@outlook.com>
2020-03-14 21:24:40 +0100

    small MarkerLimits refactoring

--- src/js/Mixins/MarkerLimits.js
+++ src/js/Mixins/MarkerLimits.js
@@ -1,40 +1,50 @@
 const MarkerLimits = {
   filterMarkerGroup() {
     // don't do it if the option is disabled
-    if (this.options.limitMarkersToCount === -1) {
-      return;
-    }
 
+    // define cache
     this.markerCache = [];
     this.refreshMarkerCache();
 
+    // refresh cache when layer was edited (e.g. when a vertex was added or removed)
     this._layer.on('pm:edit', () => {
       this.refreshMarkerCache();
     })
 
+    // remove events when edit mode is disabled
     this._layer.on('pm:disable', () => {
-      // remove markerFilter mousemove event
       this._map.off('mousemove', this._filterClosestMarkers, this);
     });
 
-    this.markerCache.forEach((l) => {
-      this._markerGroup.removeLayer(l)
-    })
+
 
   },
   refreshMarkerCache() {
+    // get all markers
     const allMarkers = [...this._markerGroup.getLayers(), ...this.markerCache];
+
+    // remove duplicates
     this.markerCache = allMarkers.filter((v, i, s) => s.indexOf(v) === i);
 
-    this._map.off('mousemove', this._filterClosestMarkers, this);
-    this._map.on('mousemove', this._filterClosestMarkers, this);
-  },
-  _filterClosestMarkers({ latlng }) {
+    // When an option is set, remove all markers now
+    if (this.options.limitMarkersToCount > -1) {
+      this.markerCache.forEach((l) => {
+        this._markerGroup.removeLayer(l)
+      })
+    }
 
+    // add markers closest to the mouse
+    if (this.options.limitMarkersToCount > -1) {
+      this._map.off('mousemove', this._filterClosestMarkers, this);
+      this._map.on('mousemove', this._filterClosestMarkers, this);
+    }
+  },
+
+  _filterClosestMarkers({ latlng }) {
     this.markerCache.sort((l, t) => {
       const distanceA = l._latlng.distanceTo(latlng);
       const distanceB = t._latlng.distanceTo(latlng);
-      // console.log(distanceA);
+
       return distanceA - distanceB;
     })
 
