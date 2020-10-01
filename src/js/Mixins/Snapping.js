1b8bff4345354c35b4c9fd02a076b6ec07d2d78b
Falke Design <design.falke@gmail.com>
2020-10-01 09:30:05 +0200

    check if layer has coords (deep level) before add it to snap list (#667) (patch)
    * check if layer has coords (deep level) before add it to snap list
    
    * add test

--- src/js/Mixins/Snapping.js
+++ src/js/Mixins/Snapping.js
@@ -1,4 +1,5 @@
 import Utils from '../L.PM.Utils';
+import {isEmptyDeep} from "../helpers";
 
 const SnapMixin = {
   _initSnappableMarkers() {
@@ -257,7 +258,7 @@ const SnapMixin = {
 
     // also remove everything that has no coordinates yet
     layers = layers.filter(
-      layer => layer._latlng || (layer._latlngs && layer._latlngs.length > 0)
+      layer => layer._latlng || (layer._latlngs && !isEmptyDeep(layer._latlngs))
     );
 
     // finally remove everything that's leaflet-geoman specific temporary stuff
