313e9284c6351fc341ce267fa06b1de910be983a
Falke Design <design.falke@gmail.com>
2021-06-08 19:21:42 +0200

    Fire `pm:edit` after rotation (#928) (patch)
    * Fire `pm:edit` after rotation
    
    * Change pm:edit Source to Rotation

--- src/js/Mixins/Rotating.js
+++ src/js/Mixins/Rotating.js
@@ -82,6 +82,7 @@ const RotateMixin = {
 
     this._fireRotationEnd(this._rotationLayer,startAngle,originLatLngs);
     this._fireRotationEnd(this._map,startAngle,originLatLngs);
+    this._rotationLayer.pm._fireEdit(this._rotationLayer,"Rotation");
 
     this._preventRenderingMarkers(false);
   },
