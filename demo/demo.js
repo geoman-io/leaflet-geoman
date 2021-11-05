5f34ed47870104036f8521be3d8fc0e29982ec2e
Quentin Pinçon <qpincon@masae-analytics.com>
2021-11-05 15:47:09 +0100

    restore demo page and package-lock

--- demo/demo.js
+++ demo/demo.js
@@ -25,12 +25,9 @@ const tiles3 = L.tileLayer(
   }
 );
 
-const map2 = L.map('example2', {preferCanvas: false}).setView([51.505, -0.09], 13).addLayer(tiles1);
-map2.on('touchstart', () => {
-  console.log('alo')
-})
-// const map3 = L.map('example3').setView([51.505, -0.09], 13).addLayer(tiles2);
-// const map4 = L.map('example4').setView([51.505, -0.09], 13).addLayer(tiles3);
+const map2 = L.map('example2').setView([51.505, -0.09], 13).addLayer(tiles1);
+const map3 = L.map('example3').setView([51.505, -0.09], 13).addLayer(tiles2);
+const map4 = L.map('example4').setView([51.505, -0.09], 13).addLayer(tiles3);
 // map2.dragging.disable();
 
 // map2.on('pm:create', function(e) {
@@ -61,16 +58,16 @@ const m1 = L.circleMarker([51.50313, -0.091223], { radius: 10 });
 const m2 = L.marker([51.50614, -0.0989]);
 const m3 = L.marker([51.50915, -0.096112], { pmIgnore: true });
 
-// const mGroup = L.layerGroup([m1, m2, m3]).addTo(map2);
+const mGroup = L.layerGroup([m1, m2, m3]).addTo(map2);
 // mGroup.pm.enable();
 
-// map2.pm.addControls({
-//   drawMarker: false,
-//   drawPolygon: true,
-//   editMode: false,
-//   drawPolyline: false,
-//   removalMode: true,
-// });
+map2.pm.addControls({
+  drawMarker: false,
+  drawPolygon: true,
+  editMode: false,
+  drawPolyline: false,
+  removalMode: true,
+});
 // map2.pm.addControls({
 //     drawMarker: false,
 //     drawPolygon: true,
@@ -91,7 +88,6 @@ map2.pm.addControls({
   editMode: true,
   drawPolyline: true,
   removalMode: true,
-  // oneBlock: true,
 });
 
 // map2.pm.disableDraw('Polygon');
@@ -104,7 +100,7 @@ map2.pm.addControls({
 // map2.pm.enableDraw('Polygon', { allowSelfIntersection: false });
 
 map2.on('pm:globaleditmodetoggled', function (e) {
-  console.log(e);
+  // console.log(e);
 });
 
 // GEOSJON EXAMPLE
@@ -195,32 +191,33 @@ const geoJsonData = {
   ],
 };
 
-// const theCollection = L.geoJson(geoJsonData, {
-//   pointToLayer: (feature, latlng) => {
-//     if (feature.properties.customGeometry) {
-//       return new L.Circle(latlng, feature.properties.customGeometry.radius);
-//     } else {
-//       return new L.Marker(latlng);
-//     }
-//   },
-//   // onEachFeature: (feature, layer) => {
-//   //     layer.addTo(map2);
-//   // },
-// });
+const theCollection = L.geoJson(geoJsonData, {
+  pointToLayer: (feature, latlng) => {
+    if (feature.properties.customGeometry) {
+      return new L.Circle(latlng, feature.properties.customGeometry.radius);
+    } else {
+      return new L.Marker(latlng);
+    }
+  },
+  // onEachFeature: (feature, layer) => {
+  //     layer.addTo(map2);
+  // },
+});
 
-// theCollection.addTo(map2);
+theCollection.addTo(map2);
 
-// const b = theCollection.getBounds();
-// map2.fitBounds(b);
+const b = theCollection.getBounds();
+map2.fitBounds(b);
 
+console.log(theCollection);
 
-// theCollection.on('pm:edit', function (e) {
-//   console.log(e);
-// });
+theCollection.on('pm:edit', function (e) {
+  console.log(e);
+});
 
-// theCollection.on('pm:dragstart', function (e) {
-//   console.log(e);
-// });
+theCollection.on('pm:dragstart', function (e) {
+  console.log(e);
+});
 
 // const geoJsonButton = document.getElementById('test-geojson');
 // const geoJsonLayer = L.geoJson(null, { pmIgnore: false });
@@ -232,58 +229,58 @@ const geoJsonData = {
 //     snappable: true,
 // });
 
-// map3.pm.addControls({
-//   drawMarker: true,
-//   drawPolygon: true,
-//   editMode: true,
-//   removalMode: true,
-//   drawPolyline: true,
-// });
+map3.pm.addControls({
+  drawMarker: true,
+  drawPolygon: true,
+  editMode: true,
+  removalMode: true,
+  drawPolyline: true,
+});
 
-// const markerStyle = {
-//   opacity: 0.5,
-//   draggable: false,
-// };
+const markerStyle = {
+  opacity: 0.5,
+  draggable: false,
+};
 
-// map3.pm.enableDraw('Polygon', {
-//   snappable: true,
-//   templineStyle: {
-//     color: 'blue',
-//   },
-//   hintlineStyle: {
-//     color: 'blue',
-//     dashArray: [5, 5],
-//   },
-//   pathOptions: {
-//     color: 'red',
-//     fillColor: 'orange',
-//     fillOpacity: 0.7,
-//   },
-//   markerStyle: markerStyle,
-//   cursorMarker: false,
-//   // finishOn: 'contextmenu',
-//   finishOnDoubleClick: true,
-// });
+map3.pm.enableDraw('Polygon', {
+  snappable: true,
+  templineStyle: {
+    color: 'blue',
+  },
+  hintlineStyle: {
+    color: 'blue',
+    dashArray: [5, 5],
+  },
+  pathOptions: {
+    color: 'red',
+    fillColor: 'orange',
+    fillOpacity: 0.7,
+  },
+  markerStyle: markerStyle,
+  cursorMarker: false,
+  // finishOn: 'contextmenu',
+  finishOnDoubleClick: true,
+});
 
-// var scotland = L.polygon([
-//   [
-//     [60, -13],
-//     [60, 0],
-//     [50, 4],
-//     [50, -13],
-//   ],
-//   [
-//     [55.7, -4.5],
-//     [56, -4.5],
-//     [56, -4],
-//     [55.7, -4],
-//   ],
-// ]);
-// scotland.addTo(map3);
+var scotland = L.polygon([
+  [
+    [60, -13],
+    [60, 0],
+    [50, 4],
+    [50, -13],
+  ],
+  [
+    [55.7, -4.5],
+    [56, -4.5],
+    [56, -4],
+    [55.7, -4],
+  ],
+]);
+scotland.addTo(map3);
 
-// const bounds = scotland.getBounds();
+const bounds = scotland.getBounds();
 
-// map3.fitBounds(bounds);
+map3.fitBounds(bounds);
 
 // geoJsonLayer.addEventListener('click', function(e) {
 //     geoJsonLayer.pm.toggleEdit();
@@ -292,48 +289,21 @@ const geoJsonData = {
 // geoJsonLayer.on('pm:drag', function(e) {
 //     console.log(e);
 // });
-// const curve = L.curve([ "M", [ 51.518382078677675, -0.08222579956054689 ], "L", [ 51.50171532651141, -0.08977890014648438 ], "C", [ 51.50171532651141, -0.08977890014648438 ], [ 51.51645930305757, -0.06351470947265626 ], [ 51.502677035642726, -0.06265640258789064 ], "C", [ 51.48889476822788, -0.061798095703125014 ], [ 51.49359341785831, -0.06986618041992189 ], [ 51.49359341785831, -0.06986618041992189 ] ])
-// .addTo(map2);
-// map2.on('pm:drawstart', function (e) {
-//   console.log('drawstart', e);
-// });
-// map2.on('pm:drawend', function (e) {
-//   console.log('drawend', e);
-// });
-// map2.on('pm:create', function (e) {
-//   console.log('create', e);
-// });
-// curve.on('pm:vertexadded', function (e) {
-//   console.log('vertexadded', e);
-// });
 
-// map2.on('touchstart', () => {
-//   console.log('touchstart');
-// });
-// map2.on('mousedown', () => {
-//   console.log('mousedown');
-// });
-// map2.on('click', () => {
-//   console.log('click');
-// });
-// curve.on('pm:snapdrag', function (e) {
-//   console.log('snapdrag', e);
-// });
-// curve.on('pm:snap', function (e) {
-//   console.log('snap', e);
-// });
-// curve.on('pm:unsnap', function (e) {
-//   console.log('unsnap', e);
-// });
-// map2.on('layeradd', function (e) {
-//   console.log('add', e);
-// });
-// map2.on('layerremove', function (e) {
-//   console.log('remove', e);
-// });
-
-
-
+map2.on('pm:drawstart', function (e) {
+  var layer = e.workingLayer;
+  // console.log(layer);
+  layer.on('pm:centerplaced', function (e) {
+    // console.log(e);
+  });
+});
+map2.on('pm:create', function (e) {
+  var layer = e.layer;
+  // console.log(layer);
+  layer.on('pm:centerplaced', function (e) {
+    // console.log(e);
+  });
+});
 
 // Polygon Example
 
@@ -342,8 +312,8 @@ const polygonLayer = L.polygon([
   [51.503, -0.06],
   [51.51, -0.047],
 ])
-  // .addTo(map3)
-  // .addTo(map2);
+  .addTo(map3)
+  .addTo(map2);
 
 // polygonLayer.pm.toggleEdit({
 //     allowSelfIntersection: false,
@@ -351,11 +321,11 @@ const polygonLayer = L.polygon([
 //     preventMarkerRemoval: false,
 // });
 
-map2.on('pm:update', function (e) {
+polygonLayer.on('pm:update', function (e) {
   console.log(e);
 });
 
-map2.on('pm:intersect', function (e) {
+polygonLayer.on('pm:intersect', function (e) {
   console.log(e);
 });
 
@@ -366,12 +336,10 @@ map2.on('pm:intersect', function (e) {
 // });
 // map2.pm.disableGlobalEditMode();
 
-// map2.pm.enableDraw('Polygon', { allowSelfIntersection: false });
-// map2.pm.disableDraw('Polygon');
-// map2.pm.enableDraw('Line', { allowSelfIntersection: false });
-// map2.pm.disableDraw('Line');
-// map2.pm.enableDraw('Curve', { allowSelfIntersection: true });
-// map2.pm.disableDraw('Curve');
+map2.pm.enableDraw('Polygon', { allowSelfIntersection: false });
+map2.pm.disableDraw('Polygon');
+map2.pm.enableDraw('Line', { allowSelfIntersection: false });
+map2.pm.disableDraw('Line');
 
 map2.on('pm:create', function (e) {
   // e.layer.pm.enable({ allowSelfIntersection: false });
@@ -449,40 +417,40 @@ const feature = {
   },
 };
 
-// const layerGroup = L.featureGroup([layerGroupItem1]).addTo(map4);
-// layerGroup.pm.toggleEdit({
-//   draggable: true,
-//   snappable: true,
-//   snapDistance: 30,
-// });
-// const someLayer = L.geoJSON(feature);
+const layerGroup = L.featureGroup([layerGroupItem1]).addTo(map4);
+layerGroup.pm.toggleEdit({
+  draggable: true,
+  snappable: true,
+  snapDistance: 30,
+});
+const someLayer = L.geoJSON(feature);
 
-// layerGroup.addLayer(someLayer);
+layerGroup.addLayer(someLayer);
 
-// someLayer.addData(feature);
+someLayer.addData(feature);
 
-// layerGroup.on('pm:snap', function (e) {
-//   console.log('snap');
-//   console.log(e);
-// });
-// layerGroup.on('pm:unsnap', function (e) {
-//   console.log('unsnap');
-//   console.log(e);
-// });
+layerGroup.on('pm:snap', function (e) {
+  console.log('snap');
+  console.log(e);
+});
+layerGroup.on('pm:unsnap', function (e) {
+  console.log('unsnap');
+  console.log(e);
+});
 
-// map4.pm.addControls({
-//   position: 'topright',
-// });
+map4.pm.addControls({
+  position: 'topright',
+});
 
-// map4.pm.enableDraw('Polygon', {
-//   finishOn: 'mouseout',
-// });
-// map4.pm.disableDraw('Polygon');
+map4.pm.enableDraw('Polygon', {
+  finishOn: 'mouseout',
+});
+map4.pm.disableDraw('Polygon');
 
-// map4.pm.enableDraw('Marker', {
-//   snappable: false,
-// });
-// map4.pm.disableDraw('Marker');
+map4.pm.enableDraw('Marker', {
+  snappable: false,
+});
+map4.pm.disableDraw('Marker');
 
 // map4.pm.setPathOptions({
 //     color: 'orange',
@@ -490,26 +458,26 @@ const feature = {
 //     fillOpacity: 0.4,
 // });
 
-// layerGroup.addLayer(layerGroupItem2);
-// layerGroup.addLayer(layerGroupItem3);
+layerGroup.addLayer(layerGroupItem2);
+layerGroup.addLayer(layerGroupItem3);
 // layerGroup.addLayer(layerGroupItem4);
 // layerGroup.addLayer(layerGroupItem5);
 
-// layerGroup.on('pm:dragstart', function (e) {
-//   console.log(e);
-// });
-// layerGroup.on('pm:drag', function (e) {
-//   console.log(e);
-// });
-// layerGroup.on('pm:dragend', function (e) {
-//   console.log(e);
-// });
-// layerGroup.on('pm:markerdragstart', function (e) {
-//   console.log(e);
-// });
-// layerGroup.on('pm:markerdragend', function (e) {
-//   console.log(e);
-// });
+layerGroup.on('pm:dragstart', function (e) {
+  console.log(e);
+});
+layerGroup.on('pm:drag', function (e) {
+  console.log(e);
+});
+layerGroup.on('pm:dragend', function (e) {
+  console.log(e);
+});
+layerGroup.on('pm:markerdragstart', function (e) {
+  console.log(e);
+});
+layerGroup.on('pm:markerdragend', function (e) {
+  console.log(e);
+});
 
 // test with markercluster
 // var markers = L.markerClusterGroup();
