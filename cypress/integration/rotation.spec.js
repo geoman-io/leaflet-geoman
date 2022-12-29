6bacbe43e38287e3eb2281d5da5d1c6d09db4a70
Florian Bischof <design.falke@gmail.com>
2022-12-29 18:51:36 +0100

    Fix rotation of new added layer (#1270) (patch)

--- cypress/integration/rotation.spec.js
+++ cypress/integration/rotation.spec.js
@@ -207,4 +207,17 @@ describe('Rotation', () => {
       expect(!!rotatePoly._map).to.eq(false);
     });
   });
+
+  it.only('rotate a new added layer', () => {
+    cy.window().then(({ map, L }) => {
+      expect(() => {
+        const coords = [
+          [1, 2],
+          [3, 4],
+        ];
+        const rect = L.rectangle(coords).addTo(map);
+        rect.pm.rotateLayer(50);
+      }).to.not.throw();
+    });
+  });
 });
