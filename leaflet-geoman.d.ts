19fc12b819ad1352b1bb878061683920be7444a2
Erdogan Abacı <43154455+erdoganabaci@users.noreply.github.com>
2022-12-29 18:25:41 +0100

    Change rotateEnabled return type void to boolean (#1263) (#1264) (patch)

--- leaflet-geoman.d.ts
+++ leaflet-geoman.d.ts
@@ -867,8 +867,8 @@ declare module 'leaflet' {
       /** Disables rotate mode on the layer. */
       disableRotate(): void;
 
-      /** Toggles rotate mode on the layer. */
-      rotateEnabled(): void;
+      /** Returns if rotate mode is enabled for the layer. */
+      rotateEnabled(): boolean;
 
       /** Rotates the layer by x degrees. */
       rotateLayer(degrees: number): void;
