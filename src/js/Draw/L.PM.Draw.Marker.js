5ab013622100363489133f25b25ae15fad77642d
Joonas <joonas.aapro@mainiotech.fi>
2025-10-17 14:09:35 +0300

    Remove hintMarker in case of touch screen instead of hiding it / Create a separate class function for 'updateHintPosition'

--- src/js/Draw/L.PM.Draw.Marker.js
+++ src/js/Draw/L.PM.Draw.Marker.js
@@ -198,34 +198,12 @@ Draw.Marker = Draw.extend({
   },
   _setupTouchScreenTips() {
     let markerHintTip = null;
-    const updateHintPosition = () => {
-      if (!markerHintTip) {
-        return;
-      }
 
-      const bounds = this._map.getBounds();
-
-      // Calculate the longitude distance of the map's visible area. This allows
-      // for an easy way to figure out the center point of the map.
-      const distance = this._map.distance(
-        bounds.getNorthWest(),
-        bounds.getNorthEast()
-      );
-
-      // Calculate new bounds with the current latitude distance of the visible
-      // map from the northwest point of the map. Within these new bounds, the
-      // east border is at the center of the map.
-      const newBounds = bounds.getNorthWest().toBounds(distance);
-
-      // Set the hint at the top center of the map.
-      markerHintTip.setLatLng([bounds.getNorth(), newBounds.getEast()]);
-    };
     this._map.on('pm:drawstart', (event) => {
       if (event.shape === 'Marker' && !deviceHasFinePointer()) {
         // Hides the "hint marker" as that is confusing for touch screen users.
         const drawMarker = this._map.pm.Draw.Marker;
-        drawMarker._hintMarker.setOpacity(0);
-        drawMarker._hintMarker.closeTooltip();
+        drawMarker._hintMarker.remove();
 
         // Creates the touch screen hint "permanently" (while using the tool)
         // sticked at the top of the map.
@@ -236,16 +214,44 @@ Draw.Marker = Draw.extend({
           permanent: true,
         }).addTo(this._map);
 
-        updateHintPosition();
-        this._map.on('zoomlevelschange resize move', updateHintPosition, this);
+        const updateHandler = () => this._updateHintPosition(markerHintTip);
+
+        updateHandler();
+        this._map.on('zoomlevelschange resize move', updateHandler, this);
       }
     });
     this._map.on('pm:drawend', (event) => {
       if (event.shape === 'Marker' && markerHintTip) {
         markerHintTip.remove();
         markerHintTip = null;
-        this._map.off('zoomlevelschange resize move', updateHintPosition, this);
+        this._map.off(
+          'zoomlevelschange resize move',
+          this._updateHintPosition,
+          this
+        );
       }
     });
+  },
+  _updateHintPosition(markerHintTip) {
+    if (!markerHintTip) {
+      return;
+    }
+
+    const bounds = this._map.getBounds();
+
+    // Calculate the longitude distance of the map's visible area. This allows
+    // for an easy way to figure out the center point of the map.
+    const distance = this._map.distance(
+      bounds.getNorthWest(),
+      bounds.getNorthEast()
+    );
+
+    // Calculate new bounds with the current latitude distance of the visible
+    // map from the northwest point of the map. Within these new bounds, the
+    // east border is at the center of the map.
+    const newBounds = bounds.getNorthWest().toBounds(distance);
+
+    // Set the hint at the top center of the map.
+    markerHintTip.setLatLng([bounds.getNorth(), newBounds.getEast()]);
   },
 });
