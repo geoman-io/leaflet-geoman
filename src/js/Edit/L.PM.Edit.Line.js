7f1c2dea9f431252f00b822199a412dc06b57bae
Florian Bischof <design.falke@gmail.com>
2025-11-08 17:09:21 +0100

    Fix intersection handling allowSelfIntersectionEdit

--- src/js/Edit/L.PM.Edit.Line.js
+++ src/js/Edit/L.PM.Edit.Line.js
@@ -638,27 +638,41 @@ Edit.Line = Edit.extend({
     const prevLine = L.polyline([prevMarker.getLatLng(), marker.getLatLng()]);
     const nextLine = L.polyline([marker.getLatLng(), nextMarker.getLatLng()]);
 
-    let prevLineIntersectionLen = lineIntersect(
+    const prevLineIntersection = lineIntersect(
       this._layer.toGeoJSON(15),
       prevLine.toGeoJSON(15)
-    ).features.length;
-    let nextLineIntersectionLen = lineIntersect(
+    );
+
+    // Filter out intersections that are the markers themselves
+    const prevLineIntersectionLen = prevLineIntersection.features.filter(
+      (f) => {
+        const coords = f.geometry.coordinates;
+        const latlng = L.latLng(coords[1], coords[0]);
+        return (
+          !latlng.equals(prevMarker.getLatLng()) &&
+          !latlng.equals(marker.getLatLng())
+        );
+      }
+    ).length;
+
+    const nextLineIntersection = lineIntersect(
       this._layer.toGeoJSON(15),
       nextLine.toGeoJSON(15)
-    ).features.length;
+    );
 
-    // The first and last line has one intersection fewer because they are not connected
-    if (marker.getLatLng() === this._markers[0][0].getLatLng()) {
-      nextLineIntersectionLen += 1;
-    } else if (
-      marker.getLatLng() ===
-      this._markers[0][this._markers[0].length - 1].getLatLng()
-    ) {
-      prevLineIntersectionLen += 1;
-    }
+    // Filter out intersections that are the markers themselves
+    const nextLineIntersectionLen = nextLineIntersection.features.filter(
+      (f) => {
+        const coords = f.geometry.coordinates;
+        const latlng = L.latLng(coords[1], coords[0]);
+        return (
+          !latlng.equals(nextMarker.getLatLng()) &&
+          !latlng.equals(marker.getLatLng())
+        );
+      }
+    ).length;
 
-    // <= 2 the start and end point of the line always intersecting because they have the same coords.
-    if (prevLineIntersectionLen <= 2 && nextLineIntersectionLen <= 2) {
+    if (prevLineIntersectionLen < 1 && nextLineIntersectionLen < 1) {
       return false;
     }
     return true;
--- src/js/Edit/L.PM.Edit.Polygon.js
+++ src/js/Edit/L.PM.Edit.Polygon.js
@@ -9,17 +9,41 @@ Edit.Polygon = Edit.Line.extend({
     const prevLine = L.polyline([prevMarker.getLatLng(), marker.getLatLng()]);
     const nextLine = L.polyline([marker.getLatLng(), nextMarker.getLatLng()]);
 
-    const prevLineIntersectionLen = lineIntersect(
+    const prevLineIntersection = lineIntersect(
       this._layer.toGeoJSON(15),
       prevLine.toGeoJSON(15)
-    ).features.length;
-    const nextLineIntersectionLen = lineIntersect(
+    );
+
+    // Filter out intersections that are the markers themselves
+    const prevLineIntersectionLen = prevLineIntersection.features.filter(
+      (f) => {
+        const coords = f.geometry.coordinates;
+        const latlng = L.latLng(coords[1], coords[0]);
+        return (
+          !latlng.equals(prevMarker.getLatLng()) &&
+          !latlng.equals(marker.getLatLng())
+        );
+      }
+    ).length;
+
+    const nextLineIntersection = lineIntersect(
       this._layer.toGeoJSON(15),
       nextLine.toGeoJSON(15)
-    ).features.length;
+    );
 
-    // <= 2 the start and end point of the line always intersecting because they have the same coords.
-    if (prevLineIntersectionLen <= 2 && nextLineIntersectionLen <= 2) {
+    // Filter out intersections that are the markers themselves
+    const nextLineIntersectionLen = nextLineIntersection.features.filter(
+      (f) => {
+        const coords = f.geometry.coordinates;
+        const latlng = L.latLng(coords[1], coords[0]);
+        return (
+          !latlng.equals(nextMarker.getLatLng()) &&
+          !latlng.equals(marker.getLatLng())
+        );
+      }
+    ).length;
+
+    if (prevLineIntersectionLen < 1 && nextLineIntersectionLen < 1) {
       return false;
     }
     return true;
